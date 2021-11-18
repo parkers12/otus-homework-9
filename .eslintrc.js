a9ee85b3cfbaa5984ff3cdde309850d45e3bc347
PARKER\Parker <parkers@mail.ru>
2021-11-18 16:29:04 +0300

    Add typescript to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "jest/globals": true
-    },
-    "extends": [
-        "airbnb-base"
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["jest", "@typescript-eslint"],
+  rules: {
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "jest",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        'max-len': ['error', {
-            ignoreComments: true,
-        }],
-        'import/prefer-default-export': 'off',
-    },
+    "import/prefer-default-export": "off",
+  },
 };
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Game of Life
----
+## Game of Life
+
 Homework done for the course "[JavaScript Developer. Basic](https://otus.ru/lessons/javascript-basic/)"
