26345baf431872ceaf00c39b77739074d8915afd
PARKER\Parker <parkers@mail.ru>
2021-11-18 22:53:13 +0300

    Add index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  mode: process.env.NODE_ENV === "development" ? "development" : "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Game of Life",
+      template: "./index.html",
+    }),
+  ],
+  stats: {
+    children: true,
   },
 };
