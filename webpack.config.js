572bda2e3608fb5ea9d4767b55491c7707c46cb2
PARKER\Parker <parkers@mail.ru>
2021-11-18 23:12:01 +0300

    Add clean-webpack-plugin to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -25,6 +26,7 @@ module.exports = {
         },
       },
     }),
+    new CleanWebpackPlugin(),
   ],
   stats: {
     children: true,
